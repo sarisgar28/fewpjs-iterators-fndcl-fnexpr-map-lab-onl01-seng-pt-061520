14284a014dff25a615dce091366fefd1d7531362
sarisgar28 <sarisgar28@gmail.com>
2021-01-08 14:44:02 -0500

    Done.

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ const tutorials = [
 
 
 function titleCased(){
+  //1. map the sentence tutorial and give it a variable call words
   return tutorials.map(tutorial => {
       let words = tutorial.split(" ")
+    // 2. split the sentence in strings  
       let capWords = words.map(word => {
+        //3. give another variable to of capitalized words and map words(strings) into a each word
         return word[0].toUpperCase() + word.slice(1)
+        // 3. returned each word and give it an index of 0 so is the first word and slicing the word from the first word to the rest
         
       })
       return capWords.join(" ")
+      // 4. lastly putting back together the divided strings into one 
     })
   
  };
